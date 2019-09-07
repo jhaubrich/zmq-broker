071c7c1f8efbe5d64092ed2f3b8105a843d8d9f2
Jesse Haubrich <state_of_the_geos@faa.none>
2019-09-07 06:15:38 -0500

    do actual bandwidth measurements

--- src/pub_example.py
+++ src/pub_example.py
@@ -5,10 +5,12 @@ from random import randint
 if __name__ == "__main__":
     ctx = zmq.Context.instance()
     p = ctx.socket(zmq.PUB)
+    # p.connect("tcp://45.79.47.159:5555")
     p.connect("tcp://localhost:5555")
-    i = 0
 
     while True:
+        msg = '0000 ' + str(randint(1000,4095)) # 500 kbps
+        msg = '0 ' + str(randint(1,9)) # 430 kbps
         msg = 'sensor1 ' + str(randint(0,4095))
         p.send(msg.encode('utf-8'))
         time.sleep(.1)
